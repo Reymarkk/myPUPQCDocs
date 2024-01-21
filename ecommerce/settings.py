881404e7fa82a60c4b1cb4b46da1b6efe130fd38
Reymark Calexterio <reymarkcalexterio@gmail.com>
2024-01-22 07:18:25 +0800

    added requirements

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 }
 
 database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse("database_url")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 #DATABASES = {
 #    'default': {
